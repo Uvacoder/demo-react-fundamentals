a51c349faacf86f66fa64442376bd4ad31f659e6
Yudai-creator <perezheber73@gmail.com>
2022-04-05 16:19:28 -0400

    output list of pages

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,30 @@
 import {useState} from 'react'
 
 const Home = () => {
-    // let name = 'Yudai'
+    
+    const [pages, setPages] = useState([
+        {title: 'React Components' , body: 'lorem ipsum...', id: 1},
+        {title: 'JSX Syntax' , body: 'lorem ipsum...', id: 2},
+        {title: 'Using styles' , body: 'lorem ipsum...', id: 3},
+    ]);
 
-    const [name, setName] = useState('Yudai'); 
 
-    const handleClick = () => {
-        setName('Radagon')
-    }
+    // useState practice
+    // const [name, setName] = useState('Yudai'); 
+    // const handleClick = () => {
+    //     setName('Radagon')
+    // }
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            {pages.map((page) => (
+                <div className="page-prev" key={page.id}>
+                    <h2>{page.title}</h2>
+                    <p>Written by me boss</p>
+                </div>
+            ))}
+            {/* useState practice
             <p>{name}</p>
-            <button onClick={handleClick}>Click me</button>
+            <button onClick={handleClick}>Click me</button> */}
         </div>
      );
 }
