c3a6124387179fb3c1a2eda4597d1d0a395a9fbd
Yudai-creator <perezheber73@gmail.com>
2022-04-12 10:08:14 -0400

    custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,32 @@
 import {useState, useEffect} from 'react'
 import PageList from './PageList';
+import useFetch from './useFetch';
 
 const Home = () => {
     
-
+    const {data: pages, isPending} = useFetch('https://localhost:8000/pages')
     // [
     //     {title: 'React Components' , body: 'lorem ipsum...', id: 1},
     //     {title: 'JSX Syntax' , body: 'lorem ipsum...', id: 2},
     //     {title: 'Using styles' , body: 'lorem ipsum...', id: 3},
     // ]
-    const [pages, setPages] = useState(null);
+    
 
     // const handleDelete = (id) => {
     //     const newPages = pages.filter(page => page.id !== id);
     //     setPages(newPages);
     // }
 
-    useEffect(() => {
-        fetch('http://localhost:8000/pages')
-            .then(res => {
-               return res.json();
-            })
-            .then((data) => {
-                setPages(data);
-            })
-    }, []);
-
-
     // useState practice
     // const [name, setName] = useState('Yudai'); 
     // const handleClick = () => {
     //     setName('Radagon')
     // }
+
+
     return ( 
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {pages && <PageList pages={pages} title="All pages" />}
             {/* <PageList pages={pages.filter((page) => page.title === 'JSX Syntax')} title="JSX"/> */}
 
